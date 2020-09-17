f1644efbd20c1117e238d92de58933e4a1e4a2f6
Surya Prakash R <suryaprakash498@gmail.com>
2020-09-17 08:25:46 +0530

    Add files via upload

---
+++ Simple_Neuron.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1+np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1-x)
+
+training_inputs=np.array([[0,0,1],
+                            [1,1,1],
+                            [1,0,1],
+                            [0,1,1]])
+
+training_outputs=np.array([[0,1,1,0]]).T
+
+np.random.seed(1)
+
+synaptic_weights = 2* np.random.random((3,1))-1
+
+#print(synaptic_weights)
+
+for iteration in range(10000):
+    input_layer=training_inputs
+    outputs=sigmoid(np.dot(input_layer,synaptic_weights)) # x0.w0+x1.w1+x2.w2
+    #print(outputs)
+    error= training_outputs - outputs
+    #print(error)
+    adjustments = error * sigmoid_derivative(outputs)
+    synaptic_weights += np.dot(input_layer.T,adjustments)
+
+print("Synaptic Weights")
+print(synaptic_weights)
+
+print("Output")
+print(outputs)
+
+print("Error")
+print(error)
